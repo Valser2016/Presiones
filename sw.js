a83490f8b900421d7d2a6fd3ca7d6552ab69c2f3
Valser2016 <tecnicodeservicios@valserindustriales.com>
2025-11-26 11:04:30 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'presiones-cache-v8';
+const CACHE_NAME = 'presiones-cache-v9';
 const urlsToCache = [
   './',
   './index.html?v=3',
@@ -51,6 +51,7 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
 
 
 
