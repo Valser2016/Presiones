8f1759b75a5d66299ab1c04c7fcecd1c1f6866c7
Valser2016 <tecnicodeservicios@valserindustriales.com>
2025-12-17 07:20:26 -0500

    Update service worker cache version to v20

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'presiones-cache-v19';
+const CACHE_NAME = 'presiones-cache-v20';
 const urlsToCache = [
   './',
   './index.html',
@@ -51,6 +51,7 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
 
 
 
